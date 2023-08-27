7ece0d1e68bc297caea0e7a2c5f28b7b141844af
Scott Sutherland <scott.sutherland@gmail.com>
2023-08-27 15:27:17 -0500

    Set the minimum lein verison to 2.0.0

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject cheffy "0.1.0-SNAPSHOT"
   :description "Cheffy REST API"
   :url "https://github.com/suthsc/cheffy"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring "1.8.1"]
                  [integrant "0.8.0"]
