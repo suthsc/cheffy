19f2a003e481cb2a02e44930f11c1c7940c2ffde
Scott Sutherland <scott.sutherland@gmail.com>
2023-08-27 17:55:10 -0500

    End of lesson 12
    * Versioned endpoint routes properly

--- src/cheffy/server.clj
+++ src/cheffy/server.clj
@@ -2,15 +2,13 @@
   (:require [integrant.core :as ig]
             [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [cheffy.router :as router]))
 
 (defn app
   [env]
-  (ring/ring-handler
-    (ring/router
-      [["/"
-        {:get {:handler (fn [req] {:status 200
-                                   :body "Hello World!"})}}]])))
+  (router/routes env)
+  )
 
 (defmethod ig/prep-key :server/jetty
   [_ config]
