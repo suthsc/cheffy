17e12081b21395448f5c1b7560872b8a9bed0e96
Scott Sutherland <scott.sutherland@gmail.com>
2023-08-27 16:39:10 -0500

    End of lesson 10
    * Adding integrant configuration using multimethods
    * Moved component configuration into config.edn
    * Added threaded implementation to -main to leverage integrant configuration

--- src/cheffy/server.clj
+++ src/cheffy/server.clj
@@ -1,6 +1,8 @@
 (ns cheffy.server
-  (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as jetty]))
+  (:require [integrant.core :as ig]
+            [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
 (def app
   (ring/ring-handler
@@ -9,15 +11,32 @@
         {:get {:handler (fn [req] {:status 200
                                    :body "Hello World!"})}}]])))
 
-(defn start
-  []
-  (jetty/run-jetty app {:port 3000 :join? false})
-  (println "\n Server running on port 3000"))
+(defmethod ig/prep-key :server/jetty
+  [_ config]
+  (merge config {:port (Integer/parseInt (env :port))}))
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler port]}]
+  (println (str "\nServer running on port " port))
+  (jetty/run-jetty handler {:port port :join? false}))
+
+(defmethod ig/init-key :cheffy/app
+  [_ config]
+  (println "\nStarted app")
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (println "\nConfigured db")
+  (:jdbc-url config))
+
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [config-file]
+  (let [config (-> config-file slurp ig/read-string)]
+    (-> config ig/prep ig/init)))
 
 (comment
   (start)
