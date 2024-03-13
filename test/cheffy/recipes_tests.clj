c49558d5d9480e1915a4ac2ea13ee02c0b7192ba
Scott Sutherland <scott.sutherland@gmail.com>
2024-03-12 21:07:55 -0500

    After lesson 27 - Recipes Tests

---
+++ test/cheffy/recipes_tests.clj
@@ -0,0 +1,18 @@
+(ns cheffy.recipes-tests
+  (:require [clojure.test :refer :all]
+            [cheffy.server :refer :all]
+            [cheffy.test-system :as ts]))
+
+(deftest a-test
+  (testing "List recipes"
+    (testing "with auth -- public and drafts"
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes" {:auth true})]
+        (is (= 200 status))
+        (is (vector? (:public body)))
+        (is (vector? (:drafts body)))))
+
+    (testing "without auth -- public"
+      (let [{:keys [status body]} (ts/test-endpoint :get "/v1/recipes" {:auth false})]
+        (is (= 200 status))
+        (is (vector? (:public body)))
+        (is (nil? (:drafts body)))))))
