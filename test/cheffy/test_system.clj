4c0c8bd061c174af1755467e3509226ef4f565ba
Scott Sutherland <scott.sutherland@gmail.com>
2024-03-12 20:57:33 -0500

    After lesson 26 - Test endpoint

---
+++ test/cheffy/test_system.clj
@@ -0,0 +1,24 @@
+(ns cheffy.test-system
+  (:require [clojure.test :refer :all]
+            [integrant.repl.state :as state]
+            [muuntaja.core :as m]
+            [ring.mock.request :as mock]))
+
+(defn test-endpoint
+  ([method uri]
+   (test-endpoint method uri nil))
+  ([method uri opts]
+   (let [app (-> state/system :cheffy/app)
+         request (app (-> (mock/request method uri)
+                          (cond-> (:body opts) (mock/json-body (:body opts)))))]
+     (update request :body (partial m/decode "application/json")))))
+
+(comment
+  (let [request (test-endpoint :get "/v1/recipes")
+        decoded-request (m/decode-response-body request)]
+    (assoc request :body decoded-request))
+
+  (test-endpoint :post "/v1/recipes" {:img "string"
+                                      :name "my name"
+                                      :prep-time 30})
+  )
