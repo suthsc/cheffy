52816c2a54ed47e2cb0a3aa9422745f9a49e1c79
Scott Sutherland <scott.sutherland@gmail.com>
2024-03-17 16:48:23 -0500

    After lesson 34 - Developer experience

--- src/cheffy/recipe/routes.clj
+++ src/cheffy/recipe/routes.clj
@@ -19,18 +19,31 @@
                  :responses  {201 {:body {:recipe-id string?}}}
                  :summary    "Create recipe"}}]
      ["/:recipe-id"
-      {:get    {:handler    (recipe/retrieve-recipe db)
-                :parameters {:path {:recipe-id string?}}
-                :responses  {200 {:body responses/recipe}}
-                :summary    "Retrieve recipe"}
-       :put    {:handler    (recipe/update-recipe! db)
-                :middleware [[mw/wrap-recipe-owner db]]
-                :parameters {:path {:recipe-id string?}
-                             :body {:name string? :prep-time int? :public boolean? :img string?}}
-                :responses  {204 (:body nil?)}
-                :summary    "Update Recipe"}
-       :delete {:handler    (recipe/delete-recipe! db)
-                :middleware [[mw/wrap-recipe-owner db]]
-                :parameters {:path {:recipe-id string?}}
-                :responses  {204 {:body nil?}}
-                :summary    "Delete recipe"}}]]))
+      [""
+       {:get    {:handler    (recipe/retrieve-recipe db)
+                 :parameters {:path {:recipe-id string?}}
+                 :responses  {200 {:body responses/recipe}}
+                 :summary    "Retrieve recipe"}
+        :put    {:handler    (recipe/update-recipe! db)
+                 :middleware [[mw/wrap-recipe-owner db]]
+                 :parameters {:path {:recipe-id string?}
+                              :body {:name string? :prep-time int? :public boolean? :img string?}}
+                 :responses  {204 {:body nil?}}
+                 :summary    "Update Recipe"}
+        :delete {:handler    (recipe/delete-recipe! db)
+                 :middleware [[mw/wrap-recipe-owner db]]
+                 :parameters {:path {:recipe-id string?}}
+                 :responses  {204 {:body nil?}}
+                 :summary    "Delete recipe"}}]
+      ["/favorite"
+       {:post   {:handler    (recipe/delete-recipe! db)
+                 :middleware [[mw/wrap-recipe-owner db]]
+                 :parameters {:path {:recipe-id string?}
+                              :body {:name string? :prep-time int? :public boolean? :img string?}}
+                 :responses  {204 {:body nil?}}
+                 :summary    "Update Recipe"}
+        :delete {:handler    (recipe/delete-recipe! db)
+                 :middleware [[mw/wrap-recipe-owner db]]
+                 :parameters {:path {:recipe-id string?}}
+                 :responses  {204 {:body nil?}}
+                 :summary    "Delete recipe"}}]]]))
--- src/cheffy/router.clj
+++ src/cheffy/router.clj
@@ -7,25 +7,29 @@
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.coercion.spec :as coercion-spec]
             [reitit.ring.coercion :as coercion]
-            [reitit.ring.middleware.exception :as exception]))
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.dev.pretty :as pretty]
+            [reitit.ring.spec :as rs]))
 
 (def swagger-docs
   ["/swagger.json"
    {:get
-    {:no-doc true
+    {:no-doc  true
      :swagger {:basePath "/"
-               :info {:title "Cheffy API Reference"
-                      :description "The Cheffy API is organized around REST. Returns JSON, Transit (msgpack, json) or EDN encoded responses."
-                      :version "1.0.0"}}
+               :info     {:title       "Cheffy API Reference"
+                          :description "The Cheffy API is organized around REST. Returns JSON, Transit (msgpack, json) or EDN encoded responses."
+                          :version     "1.0.0"}}
      :handler (swagger/create-swagger-handler)}}])
 (def router-config
-  {:data {:coercion coercion-spec/coercion
-          :muuntaja   m/instance
-          :middleware [swagger/swagger-feature
-                       muuntaja/format-middleware
-                       #_ exception/exception-middleware
-                       coercion/coerce-request-middleware
-                       coercion/coerce-response-middleware]}})
+  {:exception pretty/exception
+   :data      {:coercion   coercion-spec/coercion
+               :muuntaja   m/instance
+               :middleware [swagger/swagger-feature
+                            muuntaja/format-middleware
+                            exception/exception-middleware
+                            coercion/coerce-request-middleware
+                            coercion/coerce-response-middleware]}
+   :validate  rs/validate})
 
 (defn routes
   [env]
